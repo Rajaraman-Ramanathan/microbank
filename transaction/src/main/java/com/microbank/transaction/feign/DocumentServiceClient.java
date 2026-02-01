8dc5feb08790bd9542ab8b38a6ead28e7e8c2848
Rajaraman-Ramanathan <rajaramanamj@gmail.com>
2026-02-01 10:48:18 +0530

    added import for requestparam

--- transaction/src/main/java/com/microbank/transaction/feign/DocumentServiceClient.java
+++ transaction/src/main/java/com/microbank/transaction/feign/DocumentServiceClient.java
@@ -5,6 +5,7 @@ import com.microbank.transaction.config.FeignMultipartConfig;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
