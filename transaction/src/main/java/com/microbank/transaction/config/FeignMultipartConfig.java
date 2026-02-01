ca69d64844dd7d8daa9931875b7dd3c157c0066c
Rajaraman-Ramanathan <rajaramanamj@gmail.com>
2026-02-01 10:20:34 +0530

    Adding configuration annotation to fix multipart issue

--- transaction/src/main/java/com/microbank/transaction/config/FeignMultipartConfig.java
+++ transaction/src/main/java/com/microbank/transaction/config/FeignMultipartConfig.java
@@ -3,11 +3,13 @@ package com.microbank.transaction.config;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.ObjectFactory;
 import feign.codec.Encoder;
 
 import feign.form.spring.SpringFormEncoder;
 
+@Configuration
 public class FeignMultipartConfig {
 
     @Bean
